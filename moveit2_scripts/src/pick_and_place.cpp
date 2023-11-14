395f27e3e4324a321c01c2d2728b0e21a4a285d1
leokim0711092 <leokim10073@gmail.com>
2023-11-14 07:51:33 +0000

    Create launch file & cpp file & add the collision box into cpp file

---
+++ moveit2_scripts/src/pick_and_place.cpp
@@ -0,0 +1,251 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("move_group_demo");
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions node_options;
+  node_options.automatically_declare_parameters_from_overrides(true);
+
+  auto move_group_node =
+      rclcpp::Node::make_shared("move_group_interface_tutorial", node_options);
+
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(move_group_node);
+  std::thread([&executor]() { executor.spin(); }).detach();
+
+  static const std::string PLANNING_GROUP_ARM = "ur_manipulator";
+  static const std::string PLANNING_GROUP_GRIPPER = "gripper";
+
+  moveit::planning_interface::MoveGroupInterface move_group_arm(
+      move_group_node, PLANNING_GROUP_ARM);
+  moveit::planning_interface::MoveGroupInterface move_group_gripper(
+      move_group_node, PLANNING_GROUP_GRIPPER);
+
+  const moveit::core::JointModelGroup *joint_model_group_arm =
+      move_group_arm.getCurrentState()->getJointModelGroup(PLANNING_GROUP_ARM);
+
+  const moveit::core::JointModelGroup *joint_model_group_gripper =
+      move_group_gripper.getCurrentState()->getJointModelGroup(
+          PLANNING_GROUP_GRIPPER);
+
+  // Get Current State
+  moveit::core::RobotStatePtr current_state_arm =
+      move_group_arm.getCurrentState(10);
+  moveit::core::RobotStatePtr current_state_gripper =
+      move_group_gripper.getCurrentState(10);
+
+  std::vector<double> joint_group_positions_arm;
+  std::vector<double> joint_group_positions_gripper;
+  current_state_arm->copyJointGroupPositions(joint_model_group_arm,
+                                             joint_group_positions_arm);
+  current_state_gripper->copyJointGroupPositions(joint_model_group_gripper,
+                                                 joint_group_positions_gripper);
+
+  move_group_arm.setStartStateToCurrentState();
+  move_group_gripper.setStartStateToCurrentState();
+
+
+
+//   move_group_arm.execute(my_plan_arm);
+
+  // Create collision object for the robot to avoid
+  auto const collision_object = [frame_id =
+                                     move_group_arm.getPlanningFrame()] {
+    moveit_msgs::msg::CollisionObject collision_object;
+    collision_object.header.frame_id = frame_id;
+    collision_object.id = "box1";
+    shape_msgs::msg::SolidPrimitive primitive;
+
+    // Define the size of the box in meters
+    primitive.type = primitive.BOX;
+    primitive.dimensions.resize(3);
+    primitive.dimensions[primitive.BOX_X] = 2.64;
+    primitive.dimensions[primitive.BOX_Y] = 0.15;
+    primitive.dimensions[primitive.BOX_Z] = 2.5;
+
+    // Define the pose of the box (relative to the frame_id)
+    geometry_msgs::msg::Pose box_pose;
+    box_pose.orientation.w =
+        1.0; // We can leave out the x, y, and z components of the quaternion
+             // since they are initialized to 0
+    box_pose.position.x = 0.5;
+    box_pose.position.y = 0.55;
+    box_pose.position.z = 1.25;
+
+    collision_object.primitives.push_back(primitive);
+    collision_object.primitive_poses.push_back(box_pose);
+    collision_object.operation = collision_object.ADD;
+
+    return collision_object;
+  }();
+
+    // Table
+    auto const collision_object_table = [frame_id =
+                                     move_group_arm.getPlanningFrame()] {
+    moveit_msgs::msg::CollisionObject collision_object_1;
+    collision_object_1.header.frame_id = frame_id;
+    collision_object_1.id = "box2";
+    shape_msgs::msg::SolidPrimitive primitive_1;
+
+    // Define the size of the box in meters
+    primitive_1.type = primitive_1.BOX;
+    primitive_1.dimensions.resize(3);
+    primitive_1.dimensions[primitive_1.BOX_X] = 1.8;
+    primitive_1.dimensions[primitive_1.BOX_Y] = 0.6;
+    primitive_1.dimensions[primitive_1.BOX_Z] = 0.02;
+
+    // Define the pose of the box (relative to the frame_id)
+    geometry_msgs::msg::Pose box_pose_1;
+    box_pose_1.orientation.w =
+        1.0; // We can leave out the x, y, and z components of the quaternion
+             // since they are initialized to 0
+    box_pose_1.position.x = 0.8;
+    box_pose_1.position.y = 0.2;
+    box_pose_1.position.z = -0.011;
+
+    collision_object_1.primitives.push_back(primitive_1);
+    collision_object_1.primitive_poses.push_back(box_pose_1);
+    collision_object_1.operation = collision_object_1.ADD;
+
+    return collision_object_1;
+  }();
+
+  // Add the collision object to the scene
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface_2;
+  planning_scene_interface.applyCollisionObject(collision_object);
+  planning_scene_interface_2.applyCollisionObject(collision_object_table);
+  RCLCPP_INFO(LOGGER, "Create Collision");
+
+
+  // Go Home
+  RCLCPP_INFO(LOGGER, "Going Home");
+
+//   joint_group_positions_arm[0] = -6.32;  // Shoulder Pan
+//   joint_group_positions_arm[1] = -2.50; // Shoulder Lift
+//   joint_group_positions_arm[2] = 1.50;  // Elbow
+//   joint_group_positions_arm[3] = -1.50; // Wrist 1
+//   joint_group_positions_arm[4] = -4.0; // Wrist 2
+//   joint_group_positions_arm[5] = -1.5;  // Wrist 3
+
+//   move_group_arm.setJointValueTarget(joint_group_positions_arm);
+  move_group_arm.setNamedTarget("home");
+
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan_arm;
+  bool success_arm = (move_group_arm.plan(my_plan_arm) ==
+                      moveit::core::MoveItErrorCode::SUCCESS);
+
+  //   Pregrasp
+  RCLCPP_INFO(LOGGER, "Pregrasp Position");
+
+  geometry_msgs::msg::Pose target_pose1;
+  target_pose1.orientation.x = -1;
+  target_pose1.orientation.y = 0.0;
+  target_pose1.orientation.z = 0.0;
+  target_pose1.orientation.w = 0.0;
+  target_pose1.position.x = 0.34;
+  target_pose1.position.y = 0.02;
+  target_pose1.position.z = 0.04229;
+  move_group_arm.setPoseTarget(target_pose1);
+
+  success_arm = (move_group_arm.plan(my_plan_arm) ==
+                 moveit::core::MoveItErrorCode::SUCCESS);
+
+//   move_group_arm.execute(my_plan_arm);
+
+  //   Open Gripper
+
+  //   RCLCPP_INFO(LOGGER, "Open Gripper!");
+
+  //   move_group_gripper.setNamedTarget("open");
+
+  //   moveit::planning_interface::MoveGroupInterface::Plan my_plan_gripper;
+  //   bool success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
+  //                           moveit::core::MoveItErrorCode::SUCCESS);
+
+  //   move_group_gripper.execute(my_plan_gripper);
+
+  // Approach
+  //   RCLCPP_INFO(LOGGER, "Approach to object!");
+
+  //   std::vector<geometry_msgs::msg::Pose> approach_waypoints;
+  //   target_pose1.position.z -= 0.03;
+  //   approach_waypoints.push_back(target_pose1);
+
+  //   target_pose1.position.z -= 0.03;
+  //   approach_waypoints.push_back(target_pose1);
+
+  //   moveit_msgs::msg::RobotTrajectory trajectory_approach;
+  //   const double jump_threshold = 0.0;
+  //   const double eef_step = 0.01;
+
+  //   double fraction = move_group_arm.computeCartesianPath(
+  //       approach_waypoints, eef_step, jump_threshold, trajectory_approach);
+
+  //   move_group_arm.execute(trajectory_approach);
+
+  //   Close Gripper
+
+  //   RCLCPP_INFO(LOGGER, "Close Gripper!");
+
+  //   move_group_gripper.setNamedTarget("close");
+
+  //   success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
+  //                      moveit::core::MoveItErrorCode::SUCCESS);
+
+  //   move_group_gripper.execute(my_plan_gripper);
+
+  //   // Retreat
+
+  //   RCLCPP_INFO(LOGGER, "Retreat from object!");
+
+  //   std::vector<geometry_msgs::msg::Pose> retreat_waypoints;
+  //   target_pose1.position.z += 0.03;
+  //   retreat_waypoints.push_back(target_pose1);
+
+  //   target_pose1.position.z += 0.03;
+  //   retreat_waypoints.push_back(target_pose1);
+
+  //   moveit_msgs::msg::RobotTrajectory trajectory_retreat;
+
+  //   fraction = move_group_arm.computeCartesianPath(
+  //       retreat_waypoints, eef_step, jump_threshold, trajectory_retreat);
+
+  //   move_group_arm.execute(trajectory_retreat);
+
+  // Place
+
+  //   RCLCPP_INFO(LOGGER, "Rotating Arm");
+
+  //   current_state_arm = move_group_arm.getCurrentState(10);
+  //   current_state_arm->copyJointGroupPositions(joint_model_group_arm,
+  //                                              joint_group_positions_arm);
+
+  //   joint_group_positions_arm[0] = 1.57; // Shoulder Pan
+
+  //   move_group_arm.setJointValueTarget(joint_group_positions_arm);
+
+  //   success_arm = (move_group_arm.plan(my_plan_arm) ==
+  //                  moveit::core::MoveItErrorCode::SUCCESS);
+
+  //   move_group_arm.execute(my_plan_arm);
+
+  // Open Gripper
+
+  //   RCLCPP_INFO(LOGGER, "Release Object!");
+
+  //   move_group_gripper.setNamedTarget("open");
+
+  //   success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
+  //                      moveit::core::MoveItErrorCode::SUCCESS);
+
+  //   move_group_gripper.execute(my_plan_gripper);
+
+  rclcpp::shutdown();
+  return 0;
+}
